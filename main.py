692a299e7c33f62c52dd16b6c7a9dbd04f764548
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2025-08-21 09:50:43 -0500

    Remove temporary code and add boilerplate code

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-words_list = []
-with open("words.txt") as file:
-    file_contents = file.readlines()
-    for word in file_contents:
-        if "\n" in word:
-            word = word.replace("\n", "")
-        words_list.append(word)
+from words_list import words as possible_words
 
-with open("words.py", "w") as output:
-    output.write("words = [\n")
+def main():
+    print("meow")
 
-with open("words.py", "a") as output:
-    for i in words_list:
-        output.write(f'\t"{i}",\n')
-    output.write("]")
+if __name__ == "__main__":
+    main()

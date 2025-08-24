7f026d97853857cc52afdaadc8c9abfff2712092
Slightly-Deranged-Lunatic <110319723+Slightly-Deranged-Lunatic@users.noreply.github.com>
2025-08-24 14:17:46 -0500

    bad evil mesy code

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
 
 def get_hints():
     green_letters = list(input("Enter all green letters you know so far, use a space to show none ").lower())
-    yellow_letters = list(input("Enter all yellow letters you know so far, use a space to show none ").lower())
+    yellow_letters = []
+    for i in range(1, 6):
+        yellow_letters.append(list(input(f"Enter the yellow letters for row {i}, use spaces to show no letters ").lower()))
     grey_letters = list(input("Optional: Enter all letters without a color. This is optional but doing so will significantly improve the accuracy ").lower())
     hints = {
         "green letters" : green_letters,
@@ -36,20 +38,27 @@ def filter_yellow_letters(hints, possible_words):
     yellow_letters = hints["yellow letters"]
     new_words = list(possible_words)
     for word in possible_words:
-        for letter in yellow_letters:
-            if letter not in word and letter != " ":
-                new_words.remove(word)
+        for row in yellow_letters:
+            for yellow_letter in row:
+                if yellow_letter not in word and yellow_letter != " ":
+                    new_words.remove(word)
+                    break
+            if word not in new_words:
                 break
         if word not in new_words:
             continue
         for i in range(len(word)):
-            if word[i] == yellow_letters[i]:
-                new_words.remove(word)
-                break            
+            for row in yellow_letters:
+                if word[i] == row[i]:
+                    new_words.remove(word)
+                    break
+            if word not in new_words:
+                break
+        if word not in new_words:
+            continue
     return new_words
 
 def filter_grey_letters(hints, possible_words):
-    # Removes the words with colorless letters
     grey_letters = hints["grey letters"]
     for word in words:
         for letter in word:
